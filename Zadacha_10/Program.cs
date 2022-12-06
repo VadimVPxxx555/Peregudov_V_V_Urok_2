1097c3964779a96273a3f4cca02edadcda0c4ef3
Vadim V P <Everestxxx555@yandex.ru>
2022-12-07 02:36:32 +0300

    коммит Zadacha_10

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход 
+//трехзначное число и на выходе показывает вторую
+//цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+number = number / 10;
+int number2 =  number % 10;
+
+Console.WriteLine ($"втарая цыфра {number2} ");
