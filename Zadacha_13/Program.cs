7273b1b7b4096f1f815ad21c5e4e11880758d867
Vadim V P <Everestxxx555@yandex.ru>
2022-12-08 00:43:19 +0300

    коммит Zadacha_13 2 на 3

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -3,7 +3,7 @@
 Console.Clear();
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
-int number2 = 0;
+int number3 = 0;
 int numberGap = 0;
 
 if(number < 100)
@@ -17,8 +17,8 @@ else
  {
      number = number / 10;
  }
-   number2 = number % 10;
-   Console.WriteLine ($"{numberGap} -> {number2}");
+   number3 = number % 10;
+   Console.WriteLine ($"{numberGap} -> {number3}");
 }
 
 
