67bc4502d6948fbeb4db5666520a99c678ec037e
Vadim V P <Everestxxx555@yandex.ru>
2022-12-08 00:17:08 +0300

    коммит 10 и 13

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -7,4 +7,4 @@ int number = int.Parse(Console.ReadLine());
 number = number / 10;
 int number2 =  number % 10;
 
-Console.WriteLine ($"втарая цыфра {number2} ");
+Console.WriteLine ($"{number} -> {number2} ");
---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая выводит третью цифру
+//заданного числа или сообщает, что третьей цифры нет
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int number2 = 0;
+int numberGap = 0;
+
+if(number < 100)
+{
+    Console.WriteLine ($"{number} -> третьей цифры нет ");
+}
+else
+{
+  numberGap = number;  
+  while(number > 1000)
+ {
+     number = number / 10;
+ }
+   number2 = number % 10;
+   Console.WriteLine ($"{numberGap} -> {number2}");
+}
+
+
+
+
+// number = number / 10;
+// int number2 =  number % 10;
+
+// Console.WriteLine ($"втарая цыфра {number2} ");
