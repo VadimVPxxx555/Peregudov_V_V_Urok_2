2264d8d5e52906e584a0098778d69d12363ea6da
Vadim V P <Everestxxx555@yandex.ru>
2022-12-08 00:38:54 +0300

    коммит Zadacha_15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли
+// этот день выходным
+Console.Clear();
+Console.WriteLine("Введите номер дня недели");
+int number = int.Parse(Console.ReadLine());
+
+if(number == 6 || number == 7)
+{
+   Console.WriteLine ($"{number} -> да, выходной");
+}
+else
+{
+    Console.WriteLine ($"{number} -> нет, рабочий");
+}
